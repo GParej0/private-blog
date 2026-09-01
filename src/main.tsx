752ecd007ea4a92a1e67a9e08e65e71acadd989f
GParej0 <195317371+GParej0@users.noreply.github.com>
2026-09-01 19:09:27 +0200

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 
 import App from "./App.tsx";
 import LogIn from "./components/login.tsx";
@@ -28,6 +28,10 @@ const router = createBrowserRouter([
         path: "/",
         element: <App />,
         children: [
+          {
+            index: true,
+            element: <Navigate to="/login" replace />,
+          },
           {
             index: true,
             element: <MyPosts />,
